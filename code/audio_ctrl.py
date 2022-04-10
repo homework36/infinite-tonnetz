0834ad56f78899d168261072a8eb041bb6e68a2b
homework36 <70364944+homework36@users.noreply.github.com>
2022-04-10 15:33:23 -0400

    separate files

---
+++ code/audio_ctrl.py
@@ -0,0 +1,74 @@
+from math import radians, ceil
+from struct import calcsize
+import sys, os
+sys.path.insert(0, os.path.abspath('..'))
+
+from imslib.core import BaseWidget, run, lookup
+from imslib.gfxutil import topleft_label, resize_topleft_label, CEllipse, KFAnim, AnimGroup, CRectangle
+
+from kivy.core.window import Window
+from kivy.clock import Clock as kivyClock
+from kivy.uix.label import Label
+from kivy.graphics.instructions import InstructionGroup
+from kivy.graphics import Color, Ellipse, Rectangle, Line
+from kivy.graphics import PushMatrix, PopMatrix, Translate, Scale, Rotate
+
+
+from imslib.writer import AudioWriter
+from imslib.audio import Audio
+from imslib.clock import SimpleTempoMap, AudioScheduler, kTicksPerQuarter, quantize_tick_up
+from imslib.core import BaseWidget, run
+from imslib.gfxutil import topleft_label, resize_topleft_label, Cursor3D, AnimGroup, scale_point, CEllipse
+from imslib.leap import getLeapInfo, getLeapFrame
+from imslib.synth import Synth
+
+from random import randint, random
+import numpy as np
+# from pyrsistent import b
+from OSCReader import OSCReader
+from random import randint
+from helper_function import *
+
+
+class AudioController(object):
+    def __init__(self, song_path):
+        super(AudioController, self).__init__()
+        self.audio = Audio(2)
+        self.mixer = Mixer()
+        self.synth = Synth()
+
+        # create TempoMap, AudioScheduler
+        self.tempo_map  = SimpleTempoMap(60)
+        self.sched = AudioScheduler(self.tempo_map)
+
+        # connect scheduler into audio system
+        self.audio.set_generator(self.sched)
+        self.sched.set_generator(self.mixer)
+
+        # value for init
+        self.bass = NoteGenerator(60, 0, 'sine')
+        self.third = NoteGenerator(60, 0, 'sine')
+        self.fifth = NoteGenerator(60, 0, 'sine')
+
+        # note parameters
+        self.root_pitch = 60
+        self.pitch = 60
+        self.mode = 1
+        self.vel = 80
+
+        self.keys = ['C','C#','D','Eb','E','F','F#',\
+                'G','Ab','A','Bb','B','C']
+        self.modes = [' minor',' major']
+        self.key = self.keys[(self.pitch-60)%12] + self.modes[self.mode]
+        self.pitchlists = [(0, 2, 3, 5, 7, 8, 11, 12),\
+            (0, 2, 4, 5, 7, 9, 11, 12)]
+
+    # start / stop the song
+    def toggle(self):
+        pass
+
+
+
+    # needed to update audio
+    def on_update(self):
+        self.audio.on_update()
