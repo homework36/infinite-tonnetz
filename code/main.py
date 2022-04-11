15f9e3fa1ee257977c87880c4c81133640140c79
luyu1097 <luyu1097@gmail.com>
2022-04-10 20:06:26 -0400

    Changed position to accel, changed object icon

--- code/main.py
+++ code/main.py
@@ -12,7 +12,7 @@ from kivy.uix.label import Label
 from kivy.graphics.instructions import InstructionGroup
 from kivy.graphics import Color, Ellipse, Rectangle, Line
 from kivy.graphics import PushMatrix, PopMatrix, Translate, Scale, Rotate
-
+from kivy.uix.image import Image
 
 from imslib.writer import AudioWriter
 from imslib.audio import Audio
@@ -31,7 +31,6 @@ from helper_function import *
 from tonnetz import *
 from audio_ctrl import *
 
-
 '''
 Please make sure to quit ZIG Indicator on the computer, otherwise 
 there would be the error " OSError: [Errno 48] Address already in use"
@@ -39,45 +38,57 @@ there would be the error " OSError: [Errno 48] Address already in use"
 Please have ZIG SIM open all the time on the phone and stay on the tab "Start"
 '''
 
-
 rescale_const = Window.width / 2
-
 class PhysBubble(InstructionGroup):
-    def __init__(self, pos, r, vel, color=(1,1,1)):
+    def __init__(self, pos, r, color=(1,1,1)):
         super(PhysBubble, self).__init__()
 
         self.radius = r
-        # self.pos = np.array(pos, dtype=float)
-        # self.vel = np.array((randint(-300, 300), 0), dtype=float)
         self.pos_x, self.pos_y = pos
-        self.vel_x, self.vel_y = 0, 0
+        self.last_pos = pos
+        self.vel_x, self.vel_y = 0., 0.
+
         self.color = Color(rgb=color)
         self.add(self.color)
 
         self.circle = CEllipse(cpos=pos, csize=(2*r,2*r), segments = 40)
+        self.circle.texture = Image(source='../img/icon.png').texture
         self.add(self.circle)
-
-    # def set_pos(self, ax, ay):
-    #     self.pos[0] = (ax+1.)/2 * Window.width
-    #     self.pos[1] = (ay+1.)/2 * Window.height
-    #     self.circle.cpos = self.pos
 
     def set_accel(self, ax, ay):
         self.ax = ax
         self.ay = ay
 
-    def get_pos(self):
+    def get_last_pos(self):
+        return self.last_pos
+
+    def get_curr_pos(self):
         return [self.pos_x, self.pos_y]
 
+    def on_resize(self, win_size):
+        self.circle.csize = (2 * win_size[0] // 50,2 * win_size[0] // 50)
+
     def on_update(self, dt):
+        self.last_pos = [self.pos_x, self.pos_y]
+
         # integrate accel to get vel
         self.vel_x = self.ax * rescale_const
         self.vel_y = self.ay * rescale_const
-        print(self.vel_x, self.vel_y)
 
         # integrate vel to get pos
-        self.pos_x += self.vel_x * dt
-        self.pos_y += self.vel_y * dt
+        if self.radius <= self.pos_x + self.vel_x * dt <= Window.width - self.radius:
+            self.pos_x += self.vel_x * dt
+        elif self.radius > self.pos_x + self.vel_x * dt:
+            self.pos_x = self.radius
+        else: # self.pos_x + self.vel_x * dt > Window.width - self.radius
+            self.pos_x = Window.width - self.radius
+        
+        if self.radius <= self.pos_y + self.vel_y * dt <= Window.height - self.radius:
+            self.pos_y += self.vel_y * dt
+        elif self.radius > self.pos_y + self.vel_y * dt:
+            self.pos_y = self.radius
+        else: # self.pos_y + self.vel_y * dt > Window.height - self.radius
+            self.pos_y = Window.height - self.radius
 
         self.circle.cpos = np.array([self.pos_x, self.pos_y], dtype=float)
         return True
@@ -95,21 +106,19 @@ class MainWidget(BaseWidget):
         self.reader = OSCReader(ip, int(port))
         self.curr_pos = self.reader.get_pos()['gravity']
 
-        self.starship = PhysBubble((Window.width/2, Window.height/2), Window.width/50, (self.curr_pos['x'],self.curr_pos['y']))
-        # self.canvas.add(self.starship)
-
-        self.mainobj = None
-        # AnimGroup handles drawing, animation, and object lifetime management
-        self.objects = AnimGroup()
-        self.canvas.add(self.objects)
-        self.objects.add(self.starship)
-
         # lines
         midpoint = (width/2,height/2)
         self.color = Color(1, 1, 1)
         self.canvas.add(self.color)
         self.tonnetz = Tonnetz(150,origin=(400,400))
         self.canvas.add(self.tonnetz)
+        
+        self.starship = PhysBubble((Window.width/2, Window.height/2), Window.width/50)
+
+        # AnimGroup handles drawing, animation, and object lifetime management
+        self.objects = AnimGroup()
+        self.canvas.add(self.objects)
+        self.objects.add(self.starship)
 
 
     def on_update(self):
@@ -122,15 +131,14 @@ class MainWidget(BaseWidget):
 
         self.info.text += 'x: ' + str(round(self.curr_pos['x'], 4)) + '\n'
         self.info.text += 'y: ' + str(round(self.curr_pos['y'], 4)) + '\n'
-        self.info.text += f'position: {self.starship.get_pos()}'
+        self.info.text += f'position: {self.starship.get_curr_pos()}'
 
     def on_resize(self,win_size):
         self.tonnetz.on_resize(win_size)
         resize_topleft_label(self.info)
-
+        self.starship.on_resize(win_size)
 
     def update_pos(self):
-        self.last_pos = self.curr_pos
         self.curr_pos = self.reader.get_pos()['gravity']
         # self.curr_z = self.curr_pos['z']        
     
