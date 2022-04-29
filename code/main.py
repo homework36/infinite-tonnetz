7cc78cce1919da2d9a14d88d1a498991a291965d
luyu <luyu@mit.edu>
2022-04-29 15:12:54 -0400

    Save temp changes

--- code/main.py
+++ code/main.py
@@ -246,6 +246,7 @@ class MainWidget(BaseWidget):
 
     def on_update(self):
         if self.reader:
+            print('here')
             self.update_pos()
             self.starship.set_accel(self.curr_pos['x'], self.curr_pos['y'])
         else:
